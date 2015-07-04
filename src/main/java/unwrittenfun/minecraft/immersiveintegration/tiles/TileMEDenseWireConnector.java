53d04d1db678da6c7e1c471354866b2048a0f681
UnwrittenFun <unwrittenfun@gmail.com>
2015-07-04 01:07:57 +0100

    Report 'machine' correctly to AE for dense wire connectors.

--- src/main/java/unwrittenfun/minecraft/immersiveintegration/tiles/TileMEDenseWireConnector.java
+++ src/main/java/unwrittenfun/minecraft/immersiveintegration/tiles/TileMEDenseWireConnector.java
@@ -5,7 +5,9 @@ import appeng.api.networking.GridFlags;
 import appeng.api.util.AECableType;
 import blusunrize.immersiveengineering.api.TargetingInfo;
 import blusunrize.immersiveengineering.api.WireType;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.ForgeDirection;
+import unwrittenfun.minecraft.immersiveintegration.blocks.IIBlocks;
 import unwrittenfun.minecraft.immersiveintegration.wires.IIWires;
 
 import java.util.EnumSet;
@@ -19,5 +21,10 @@ public class TileMEDenseWireConnector extends TileMEWireConnector {
   @Override
   public EnumSet<GridFlags> getFlags() {
     return EnumSet.of(GridFlags.DENSE_CAPACITY);
+  }
+
+  @Override
+  public ItemStack getMachineRepresentation() {
+    return new ItemStack(IIBlocks.meDenseWireConnector);
   }
 }
