11e5e4bdf646bf2be5d1d52a061660aedd291447
UnwrittenFun <unwrittenfun@gmail.com>
2015-08-17 22:44:26 +0100

    Settle for horrid lighting :(

--- src/main/java/unwrittenfun/minecraft/immersiveintegration/client/renderers/TileRenderIndustrialCokeOven.java
+++ src/main/java/unwrittenfun/minecraft/immersiveintegration/client/renderers/TileRenderIndustrialCokeOven.java
@@ -3,6 +3,7 @@ package unwrittenfun.minecraft.immersiveintegration.client.renderers;
 import blusunrize.immersiveengineering.client.ClientUtils;
 import blusunrize.immersiveengineering.common.util.chickenbones.Matrix4;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -28,10 +29,10 @@ public class TileRenderIndustrialCokeOven extends TileEntitySpecialRenderer {
         GL11.glTranslated(-1, 0, 0);
       }
 
-      //      int l = tile.getWorldObj().getLightBrightnessForSkyBlocks(tile.xCoord, tile.yCoord + 3000, tile.zCoord, 0);
-      //      int l1 = l % 65536;
-      //      int l2 = l / 65536;
-      //      OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) l1, (float) l2);
+            int l = tile.getWorldObj().getLightBrightnessForSkyBlocks(tile.xCoord, tile.yCoord + 3000, tile.zCoord, 0);
+            int l1 = l % 65536;
+            int l2 = l / 65536;
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) l1, (float) l2);
 
 
       Matrix4 translationMatrix = new Matrix4();
@@ -45,7 +46,7 @@ public class TileRenderIndustrialCokeOven extends TileEntitySpecialRenderer {
       GL11.glColor4f(1, 1, 1, 1);
       ClientUtils.bindTexture(ModInfo.MOD_ID + ":textures/blocks/cokeOven.png");
       Tessellator.instance.startDrawingQuads();
-      ClientUtils.renderStaticWavefrontModel(tile, model, Tessellator.instance, translationMatrix, rotationMatrix, true, false);
+      ClientUtils.renderStaticWavefrontModel(tile, model, Tessellator.instance, translationMatrix, rotationMatrix, false, false);
       Tessellator.instance.draw();
 
       IIcon lava = Blocks.lava.getIcon(0, 0);
