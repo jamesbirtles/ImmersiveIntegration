6812de064829bdfbcd072815e28ca84868031333
UnwrittenFun <unwrittenfun@gmail.com>
2015-11-29 21:09:14 +0000

    Experimenting with having IE as a submodule on git.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git submodule update --init --recursive
+mkdir ImmersiveEngineering/libs
+curl -g -o ImmersiveEngineering/libs/CodeChickenLib-1.7.10-1.1.3.140-dev.jar http://files.minecraftforge.net/maven/codechicken/CodeChickenLib/1.7.10-1.1.3.140/CodeChickenLib-1.7.10-1.1.3.140-dev.jar
+curl -g -o ImmersiveEngineering/libs/NotEnoughItems-1.7.10-1.0.5.118-dev.jar http://chickenbones.net/maven/codechicken/NotEnoughItems/1.7.10-1.0.5.118/NotEnoughItems-1.7.10-1.0.5.118-dev.jar
+curl -g -o ImmersiveEngineering/libs/MineTweaker3-Dev-1.7.10-3.0.10B.jar http://minetweaker3.powerofbytes.com/download/MineTweaker3-Dev-1.7.10-3.0.10B.jar
+curl -g -o ImmersiveEngineering/libs/Waila-1.5.10_1.7.10-dev.jar http://mobiusstrip.eu/maven/mcp/mobius/waila/Waila/1.5.10_1.7.10/Waila-1.5.10_1.7.10-dev.jar
+curl -g -o ImmersiveEngineering/libs/forestry_1.7.10-4.2.0.47-dev.jar http://jenkins.ic2.player.to/job/Forestry/47/artifact/build/dist/forestry_1.7.10-4.2.0.47-dev.jar
+curl -g -o ImmersiveEngineering/libs/MineFactoryReloaded-[1.7.10]2.8.1B1-156-dev.jar http://addons-origin.cursecdn.com/files/2266/162/MineFactoryReloaded-[1.7.10]2.8.1B1-156-dev.jar
+curl -g -o ImmersiveEngineering/libs/HydCraft-1.7.10-2.1.242-universal.jar http://addons-origin.cursecdn.com/files/2259/816/HydCraft-1.7.10-2.1.242-universal.jar
+curl -g -o ImmersiveEngineering/libs/industrialcraft-2-2.2.801-experimental-dev.jar http://jenkins.ic2.player.to/job/IC2_experimental/801/artifact/build/libs/industrialcraft-2-2.2.801-experimental-dev.jar
+curl -g -o ImmersiveEngineering/libs/gregtech-5.09.13-dev.jar https://share.unwrittenfun.uk/gregtech-5.09.13-dev.jar
+curl -g -o ImmersiveEngineering/libs/AquaTweaks-1.7.10-1.0-deobf.jar http://addons-origin.cursecdn.com/files/2243/846/AquaTweaks-1.7.10-1.0-deobf.jar
+curl -g -o ImmersiveEngineering/libs/CoFHCore-[1.7.10]3.0.3-303-dev.jar http://addons-origin.cursecdn.com/files/2246/920/CoFHCore-[1.7.10]3.0.3-303-dev.jar
+curl -g -o ImmersiveEngineering/libs/EquivalentExchange3-1.7.10-0.3.507.jar http://ee3.pahimar.com/all/latest/EquivalentExchange3-1.7.10-0.3.507.jar
+curl -g -o ImmersiveEngineering/libs/Thaumcraft-1.7.10-4.2.3.5.jar http://addons-origin.cursecdn.com/files/2227/552/Thaumcraft-1.7.10-4.2.3.5.jar
+
+
+
+
